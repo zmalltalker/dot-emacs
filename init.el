ad75255f9475eed610fc5f5c4bfe292b59f35332
Marius Mathiesen <marius@shortcut.no>
2012-07-10 08:41:59 +0200

    Use a light-on-dark theme

--- init.el
+++ init.el
@@ -36,6 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" default)))
  '(make-backup-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

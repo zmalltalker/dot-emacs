b6c94ef0881daffadc82f9d3cccdaa4f811b32c5
Marius Mathiesen <marius@gitorious.com>
2013-05-07 08:40:04 +0200

    Try out bbatsov's zenburn for a while

--- init.el
+++ init.el
@@ -52,7 +52,7 @@ code blocks which can be tangled"
  ;; If there is more than one, they won't work right.
  '(coffee-command "/opt/node/bin/node /opt/node/bin/coffee")
  '(coffee-tab-width 2)
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "88bce56c65a570e04a01ef3908aac348448f0bb9a3922a3f595e555e8cf0705f" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" default)))
+ '(custom-safe-themes (quote ("52b5da0a421b020e2d3429f1d4929089d18a56e8e43fe7470af2cea5a6c96443" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "88bce56c65a570e04a01ef3908aac348448f0bb9a3922a3f595e555e8cf0705f" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" default)))
  '(make-backup-files nil)
  '(mu4e-mu-binary "/opt/mu/bin/mu")
  '(org-babel-load-languages (quote ((emacs-lisp . t) (ruby . t))))
